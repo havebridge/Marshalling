14acccb22563fa7f9c2cea64aeaaaa0b6b439a6f
havebridge <91760176+havebridge@users.noreply.github.com>
2022-11-11 14:46:07 +0300

    Update serialization.h

--- src/serialization.h
+++ src/serialization.h
@@ -1,26 +1,9 @@
 #pragma once
 
-#include "hashtable/test/test.h"
 #include "serialization/test/testPrimitive.h"
 #include "serialization/test/testString.h"
 #include "serialization/test/testObject.h"
 #include "serialization/test/testObject.h"
-#include "serialization/test/HashtableSerialize.h"
 
 
 #include "serialization/core.h"
-
-#define DEBUG 1
-#define RELEASE 0
-
-#if DEBUG
-
-#define TEST_PUT 0
-#define TEST_REMOVE 0
-#define TEST_GET 0
-#define TEST_PRINT 0
-#define TEST_HASH 0
-#define TEST_SERIALIZE 0
-#define TEST_DESERIALIZE 1
-
-#endif
