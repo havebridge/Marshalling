0655738839ff60c40deb9ba3b92f2ff196d779ff
havebridge <91760176+havebridge@users.noreply.github.com>
2022-11-11 14:45:28 +0300

    Update main.cpp

--- src/user/main.cpp
+++ src/user/main.cpp
@@ -6,52 +6,9 @@ int main(int argc, char** argv)
 	(void)argc;
 	(void)argv;
 	
-#if DEBUG
-	const int tableSize = 100;
-	HashTable::Hashtable<std::string, std::string, tableSize> Hashtable;
-	std::vector<HashTable::User<std::string, std::string>> Users;
-
-#if TEST_PUT
-	Users = Test::PutTest<std::string, std::string, tableSize>(Hashtable);
-#endif
-
-#if TEST_HASH
-	std::cout << Test::isGoodHashTest<std::string, std::string, tableSize>();
-#endif
-
-
-#if TEST_REMOVE
-	std::cout << Hashtable;
-	Test::RemoveTest<std::string, std::string, tableSize>(Hashtable, Users);
-	std::cout << std::endl;
-#endif
-
-#if TEST_PRINT
-	std::cout << Hashtable;
-#endif
-
-#if TEST_GET
-	std::cout << Test::GetTest<std::string, std::string, tableSize>(Hashtable);
-#endif
-
-#if TEST_SERIALIZE
-	Hashtable.Serialize();
-#endif
-
-#if TEST_DESERIALIZE
 	TestFrame::testPrimitive();
 	TestFrame::testString();
 	TestFrame::testObject();
-#endif
-
-#endif
-
-
-
-
-#if RELEASE
-	TestFrame::Test();
-#endif
 
 	return 0;
 }
