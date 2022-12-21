f5d4141c1fe98a62a42504fa46058af1887f74a9
havebridge <91760176+havebridge@users.noreply.github.com>
2022-12-21 21:35:21 +0300

    dll

--- src/serialization/object.h
+++ src/serialization/object.h
@@ -7,7 +7,7 @@
 
 namespace ObjectModel
 {
-	class Object : public Root
+	class LIB Object : public Root
 	{
 	private:
 		uint8_t primitiveCount = 0, arrayCount = 0, stringCount = 0, objectCount = 0;
